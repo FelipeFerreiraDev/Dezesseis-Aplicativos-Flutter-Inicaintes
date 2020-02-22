8d40e4e1cbef147a7ee925a216fd38acc77ffd18
FelipeFerreiraDev <felipeferreira.sp.dev@gmail.com>
2020-02-22 14:30:12 -0300

    Loja Virtual adicionando categorias

--- loja_virtual/lib/screens/home_screen.dart
+++ loja_virtual/lib/screens/home_screen.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tabs.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
-  
   final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    
     return PageView(
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
@@ -17,9 +16,20 @@ class HomeScreen extends StatelessWidget {
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
         ),
-        Container(color: Colors.red,),
-                Container(color: Colors.yellow,),
-                        Container(color: Colors.green,),
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Produtos'),
+            centerTitle: true,
+          ) ,
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.green,
+        ),
       ],
     );
   }
